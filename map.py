5920dfae51292beb3f25190ce75b31db61329ea7
napaintga <123561665+napaintga@users.noreply.github.com>
2024-09-06 18:28:21 +0300

    Update map.py

--- map.py
+++ map.py
@@ -1,43 +1,45 @@
 class Mapmanager():
     def __init__(self):
-        self.grass_block = "block/grass-block.glb"
-        self.dirt_block = "block/dirt-block.glb"
-        self.stone_block = "block/stone-block.glb"
-        
+        self.grass_block = 'grass-block.glb'
+        self.dirt_block  = "dirt-block.glb"
+        self.stone_block = "stone-block.glb"
         self.startNew()
-
-    def add_block(self, position, model):
-        self.model = loader.loadModel(model)
-        self.model.reparentTo(render)
-        self.model.setScale(1, 1, 1)
-        self.model.setPos(position)
-        self.model.setHpr(0, 90, 0)
     
+    def add_block(self,position,model):
+        self.model = loader.loadModel(model)
+        self.model.setPos(position)
+        self.model.setScale(1,1,1)
+        self.model.setHpr(0,90,0)
+        self.model.reparentTo(render)
+        
     def startNew(self):
         self.land = render.attachNewNode("Land")
+        
     
     def clear(self):
         self.land.removeNode()
         self.startNew()
-    
-    def loadland(self,file):
+        
+    def loadland(self,filename):
         self.clear()
-        with open(file) as f:
+        with open(filename) as f:
             y = 0
             for line in f:
-                x = 0 
-                line = line.split(' ')
+                x = 0
+                line = line.split(" ")
                 for z in line:
-                    for z0 in range(int(z)+1):
-                        model = self.add_block((x,y,z0), self.select_block(int(z),z0))
-                    x += 1
-                y += 1
-        return x,y
+                    for z0 in range(0,int(z)*2+2,2):
+                        model = self.add_block((x,y,(z0+4)),self.select_block(int(z)*2+2,z0))
+                    x+=2
+                y +=2
+        return x,y,int(z)
+                            
+                
     def select_block(self,z,z0):
-        if z0 < z//3:
+        if z0 <= z//3:
             return self.stone_block
-        elif z//3 < z0 < z:
+        elif  z0 > z-1:
             return self.dirt_block
         else:
-            return self.grass_block
-
+            return self.grass_block 
+        
